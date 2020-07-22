58bd5a76231284118e2055debfb8cb8ba1155ef0
Umer <an01985i@gmail.com>
2020-07-22 14:47:17 -0500

    Modified

--- exercises/Part 4-5.js
+++ exercises/Part 4-5.js
@@ -1,22 +1,19 @@
 // Part 4: Create a Spacecraft Class
-var Spacecraft = /** @class */ (function () {
+let Spacecraft = /** @class */ (function () {
     function Spacecraft(shuttleName, speed) {
         this.milesPerKilometer = 0.621;
         this.name = shuttleName;
         this.speedMph = speed;
     }
     Spacecraft.prototype.getDaysToLocation = function (kilometersAway) {
-        var milesAway = kilometersAway * this.milesPerKilometer;
-        var hoursToLocation = milesAway / this.speedMph;
+        let milesAway = kilometersAway * this.milesPerKilometer;
+        let hoursToLocation = milesAway / this.speedMph;
         return hoursToLocation / 24;
     };
     return Spacecraft;
 }());
-// I brought these in from the other typescript file, normally you would complete all of these steps in one file
-var kilometersToMars = 225000000;
-var kilometersToTheMoon = 384400;
 // Create an instance of the class here:
-var spaceShuttle = new Spacecraft('destination', 17500);
+let spaceShuttle = new Spacecraft('destination', 17500);
 // Move your output statements from part 3 here. Update the template literals use the
 // instance of the class.
 console.log(spaceShuttle.name + " would take " + spaceShuttle.getDaysToLocation(kilometersToMars) + " days to reach Mars.");
